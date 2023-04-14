9374dffb13eeb91b11e979bda5887ac3a26cdda4
Jack <jwleggett@outlook.com>
2023-04-14 17:12:22 +0100

    multi class recipe started

--- lib/user_repository.rb
+++ lib/user_repository.rb
@@ -22,19 +22,42 @@ class UserRepository
   # One argument: the id (number)
   def find(id)
     # Executes the SQL query:
-    # SELECT id, name, cohort_name FROM students WHERE id = $1;
+    sql = 'SELECT id, name, username, email_address, password FROM users WHERE id = $1;'
 
-    # Returns a single Student object.
+    result_set = DatabaseConnection.exec_params(sql, [id])
+
+    record = result_set[0]
+    user = User.new
+    user.id = record['id']
+    user.name = record['name']
+    user.username = record['username']
+    user.email_address = record['email_address']
+    user.password = record['password']
+    return user
+
   end
 
-  # Add more methods below for each operation you'd like to implement.
+  def create(user)
+    sql = 'INSERT INTO users (name, username, email_address, password) VALUES($1, $2, $3, $4);'
+    params = [user.name, user.username, user.email_address, user.password]
 
-  # def create(student)
+    DatabaseConnection.exec_params(sql, params)
+    return nil
+
+  end
+
+  # def update(user)
+  #   sql = 'INSERT INTO users (name, username, email_address, password) VALUES($1, $2, $3, $4);'
+  #   params = [user.name, user.username, user.email_address, user.password]
+
   # end
 
-  # def update(student)
-  # end
+  def delete(id)
+    sql = 'DELETE FROM users WHERE id = $1'
+    params = [id]
 
-  # def delete(student)
-  # end
+    DatabaseConnection.exec_params(sql, params)
+    return nil
+  
+  end
 end
--- spec/user_reposoitory_spec.rb
+++ spec/user_reposoitory_spec.rb
@@ -21,17 +21,19 @@ describe UserRepository do
 
     expect(users[0].id).to eq '1'
     expect(users[0].name).to eq "Jack"
+    expect(users[0].username).to eq "jack_chitter"
     expect(users[0].email_address).to eq "jack@mail.com"
     expect(users[0].password).to eq "pass1"
 
     expect(users[1].id).to eq '2'
     expect(users[1].name).to eq "Charlotte"
+    expect(users[1].username).to eq "chaz_chitter"
     expect(users[1].email_address).to eq "chaz@mail.com"
     expect(users[1].password).to eq "pass2"
 
   end
 
-  it "Get a single student" do
+  it "Get a single user" do
 
     repo = UserRepository.new
 
@@ -39,9 +41,43 @@ describe UserRepository do
 
     expect(user.id).to eq '1'
     expect(user.name).to eq "Jack"
+    expect(user.username).to eq 'jack_chitter'
     expect(user.email_address).to eq "jack@mail.com"
     expect(user.password).to eq "pass1"
   end
 
-# # Add more examples for each method
+  it "creates a user" do
+    repo = UserRepository.new
+
+    new_user = User.new
+    new_user.name = 'Ian'
+    new_user.username = 'ian_chitter'
+    new_user.email_address = 'ian@mail.com'
+    new_user.password = 'pass3'
+
+    repo.create(new_user)
+
+    user = repo.all.last
+    expect(user.id).to eq '3'
+    expect(user.name).to eq "Ian"
+    expect(user.username).to eq 'ian_chitter'
+    expect(user.email_address).to eq "ian@mail.com"
+    expect(user.password).to eq "pass3"
+  end
+
+  it "deletes a user" do
+    repo = UserRepository.new
+
+    repo.delete(1)
+    users = repo.all
+    expect(users.length).to eq 1
+  end
+
+  # it "updates a user" do
+  #   repo = UserRepository
+
+  #   user = User.new
+  #   repo.update(1)
+  # end
+
 end
