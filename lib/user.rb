de133b5eb8a442e5d224628aa115d323c8a43bf1
Jack <jwleggett@outlook.com>
2023-04-14 13:46:36 +0100

    user_reposiotry and user class's created

---
+++ lib/user.rb
@@ -0,0 +1,5 @@
+class User
+
+  # Replace the attributes by your own columns.
+  attr_accessor :id, :name, :username, :email_address, :password
+end
---
+++ spec/user_reposoitory_spec.rb
@@ -0,0 +1,41 @@
+def reset_users_table
+  seed_sql = File.read('spec/seeds_users.sql')
+  connection = PG.connect({ host: '127.0.0.1', dbname: 'chitter_database_test' })
+  connection.exec(seed_sql)
+end
+
+describe UserRepository do
+  before(:each) do 
+    reset_users_table
+  end
+
+  # 1
+# Get all students
+
+repo = UserRepository.new
+
+students = repo.all
+
+students.length # =>  2
+
+students[0].id # =>  1
+students[0].name # =>  'David'
+students[0].cohort_name # =>  'April 2022'
+
+students[1].id # =>  2
+students[1].name # =>  'Anna'
+students[1].cohort_name # =>  'May 2022'
+
+# 2
+# Get a single student
+
+repo = StudentRepository.new
+
+student = repo.find(1)
+
+student.id # =>  1
+student.name # =>  'David'
+student.cohort_name # =>  'April 2022'
+
+# Add more examples for each method
+end
